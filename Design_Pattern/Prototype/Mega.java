0f1687a11b973cf2b970c28c5c86678dd5b96f17
Jenani17 <82192805+Jenani17@users.noreply.github.com>
2022-07-20 21:54:13 +0530

    Create Mega.java

---
+++ Design_Pattern/Prototype/Mega.java
@@ -0,0 +1,21 @@
+package Prototype;
+
+public class Mega extends Icecream {
+	
+private String pack;
+		
+	public String getPack() {
+		return pack;
+	}
+	public void setPack(String pack) {
+		this.pack = pack;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Mega {" + 
+	"pack=" + pack + '}';
+	}
+	
+	}
